a093e072658b4c6e7cda4edf604733bb169c3504
mimihova <mimihova@gmail.com>
2017-06-28 12:25:18 +0300

    new files

---
+++ Dictionaries - Lab/01, Odd ocurrences/01. Odd Occurrences.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Dictionaries_Lab
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine().ToLower();
+            string[] words = input.Split(' ');
+            var result = new Dictionary<string, int>();
+            foreach (var word in words)
+            {
+                if (result.ContainsKey(word))
+                {
+                    result[word]++;
+                }
+                else
+                {
+                    result[word] = 1;
+                }
+            }
+            var results = new List<string>();
+            foreach (var kvp in result)
+            {
+                if (kvp.Value % 2 != 0)
+                {
+                    results.Add(kvp.Key);
+                }
+            }
+            Console.WriteLine(string.Join(", ", results));
+        }
+    }
+}
---
+++ Dictionaries - Lab/02. Count Real Numbers/02. Count Real Numbers.cs
@@ -0,0 +1,37 @@
+﻿
+namespace _02.Count_Real_Numbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+            var counts = new SortedDictionary<double, int>();
+
+            foreach (var num in numbers)
+            {
+                if (counts.ContainsKey(num))
+                {
+                    counts[num]++;
+                }
+                else
+                {
+                    counts[num] = 1;
+                }
+
+            }
+            foreach (var num in counts.Keys)
+            {
+
+                Console.WriteLine($"{num} -> {counts[num]}");
+
+
+            }
+        }
+    }
+}
